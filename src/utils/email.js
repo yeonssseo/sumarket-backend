7e296d2251959e82ca2fba35336cc9453790b46b
yeonssseo <dltj2541@naver.com>
2026-06-02 13:01:54 +0900

    render 서버 이메일 인증 전송 수정

--- src/utils/email.js
+++ src/utils/email.js
@@ -2,7 +2,9 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
