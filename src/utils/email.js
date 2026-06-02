48820e6aafd535ee79e017fbbd03a0acc1c89523
yeonssseo <dltj2541@naver.com>
2026-06-02 13:06:03 +0900

    다시 이메일 인증 수정

--- src/utils/email.js
+++ src/utils/email.js
@@ -5,6 +5,7 @@ const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
   secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
