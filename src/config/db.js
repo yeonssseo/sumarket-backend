7fbf0f769ca007d57c0bb1041246421cbcf58a5a
yeonssseo <dltj2541@naver.com>
2026-06-02 12:07:30 +0900

    aiven ssl 설정 추가

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,15 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
