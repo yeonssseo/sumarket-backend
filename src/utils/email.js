021ff5553698cdb20cdb015992185fe0d60ce4de
yeonssseo <dltj2541@naver.com>
2026-06-02 13:23:08 +0900

    또또 resend로 수정

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,12 +1,12 @@
-const sgMail = require('@sendgrid/mail');
+const { Resend } = require('resend');
 require('dotenv').config();
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendVerificationEmail = async (to, code) => {
-  const msg = {
+  await resend.emails.send({
+    from: 'onboarding@resend.dev',
     to,
-    from: process.env.EMAIL_USER,
     subject: '[수마켓] 이메일 인증 코드',
     html: `
       <h2>수마켓 이메일 인증</h2>
@@ -14,9 +14,7 @@ const sendVerificationEmail = async (to, code) => {
       <p>이 코드는 5분간 유효합니다.</p>
       <h1 style="letter-spacing: 8px; color: #0066cc;">${code}</h1>
     `,
-  };
-
-  await sgMail.send(msg);
+  });
 };
 
 module.exports = { sendVerificationEmail };
